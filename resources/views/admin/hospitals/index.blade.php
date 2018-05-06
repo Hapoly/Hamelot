c201b7486e19ed05fb745a3ba60af153fe638cbd
mr-exception <darbandi1996@gmail.com>
2018-05-07 00:44:59 +0430

    index and main layout added

--- resources/views/admin/hospitals/index.blade.php
+++ resources/views/admin/hospitals/index.blade.php
@@ -1,69 +1,64 @@
-@extends('layouts.app')
+@extends('layouts.main')
 @section('title', __('hospitals.index.title'))
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <a href="{{route('admin.hospitals.create')}}" class="btn btn-info" role="button">{{__('hospitals.create')}}</a>
-      </div>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <form action="{{route('admin.hospitals.index',['sort' => $sort])}}" method="get">
-          <div class="input-group">
-            <input type="text" class="form-control" name="search" value="{{old('search')}}" placeholder="{{__('hospitals.index_title')}}" aria-label="آزمون‌ها">
-            <span class="input-group-btn">
-              <button type="submit" class="btn accent-color text-primary-color">{{__('hospitals.search')}}</button>
-            </span>
-          </div>
-        </form>
-      </div>
-    </div>
-    <br />
-    @if(sizeof($hospitals))
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'id'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.row')}}</a></th>
-            <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'title'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.title')}}</a></th>
-            <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'address'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.address')}}</a></th>
-            <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'phone'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.phone')}}</a></th>
-            <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'mobile'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.mobile')}}</a></th>
-            <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'status'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.status')}}</a></th>
-            <th >{{__('hospitals.operation')}}</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($hospitals as $hospital)
-            <tr>
-            <td>{{$hospital->id}}</td>
-            <td><a href="{{route('admin.hospitals.show', ['hospital' => $hospital])}}">{{$hospital->title}}</a></td>
-            <td>{{$hospital->address}}</td>
-            <td>{{$hospital->phone}}</td>
-            <td>{{$hospital->mobile}}</td>
-            <td>{{$hospital->status}}</td>
-            <td>
-              <form action="{{route('admin.hospitals.destroy', ['hospital' => $hospital])}}" style="display: inline" method="POST" class="trash-icon">
-                {{ method_field('DELETE') }}
-                {{ csrf_field() }}
-                <button type="submit" class="btn btn-danger">{{__('hospitals.remove')}}</button>
-              </form>
-              <a href="{{route('admin.hospitals.edit', ['hospital' => $hospital])}}" class="btn btn-info" role="button">{{__('hospitals.edit')}}</a>
-            </td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
-    @else
-      <div class="row">
-        <div class="col-md-12" style="text-align: center">
-          {{__('hospitals.not_found')}}
-        </div>
-      </div>
-    @endif
-    </table>
+<div class="row" style="margin-bottom:50px;">
+  <div class="col-sm-3">
+  <a href="{{route('admin.hospitals.create')}}" class="btn add">بیمارستان<i class="fa fa-plus"></i></a>
   </div>
-  <div class="row" style="text-align: center;">
+    <div class="col-sm-9">
+      <form class="navbar-form" role="search" style="margin:auto;width:80%;direction:ltr;float:right" action="{{route('admin.hospitals.index',['sort' => $sort])}}" method="get">
+        <div class="input-group add-on">
+          <div class="input-group-btn">
+            <button class="btn" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+          </div>
+          <input class="form-control" placeholder="{{__('hospitals.search')}}" name="search" id="srch-term" value="{{old('search')}}" type="text">
+        </div>
+      </form>
+    </div>
+  </div>
+  @if(sizeof($hospitals))
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'id'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.row')}}</a></th>
+          <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'title'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.title')}}</a></th>
+          <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'address'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.address')}}</a></th>
+          <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'phone'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.phone')}}</a></th>
+          <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'mobile'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.mobile')}}</a></th>
+          <th ><a href="{{route('admin.hospitals.index',['search' => $search,'sort' => 'status'    ,'page' => $hospitals->currentPage()])}}">{{__('hospitals.status')}}</a></th>
+          <th >{{__('hospitals.operation')}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($hospitals as $hospital)
+          <tr>
+          <td>{{$hospital->id}}</td>
+          <td><a href="{{route('admin.hospitals.show', ['hospital' => $hospital])}}">{{$hospital->title}}</a></td>
+          <td>{{$hospital->address}}</td>
+          <td>{{$hospital->phone}}</td>
+          <td>{{$hospital->mobile}}</td>
+          <td>{{$hospital->status}}</td>
+          <td>
+            <form action="{{route('admin.hospitals.destroy', ['hospital' => $hospital])}}" style="display: inline" method="POST" class="trash-icon">
+              {{ method_field('DELETE') }}
+              {{ csrf_field() }}
+              <button type="submit" class="btn btn-danger">{{__('hospitals.remove')}}</button>
+            </form>
+            <a href="{{route('admin.hospitals.edit', ['hospital' => $hospital])}}" class="btn btn-info" role="button">{{__('hospitals.edit')}}</a>
+          </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  @else
+    <div class="row">
+      <div class="col-md-12" style="text-align: center">
+        {{__('hospitals.not_found')}}
+      </div>
+    </div>
+  @endif
+  <div class="container" style="text-align:center;margin-top:30px;">
     {{$hospitals->links()}}
   </div>
+</div>
 @endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title>{{ config('app.name', 'Laravel') }}</title>
+
+	<!-- Bootstrap CSS CDN -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- Our Custom CSS -->
+	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+	<div class="wrapper" class="toggled">
+		<!-- Sidebar Holder -->
+		<nav id="sidebar">
+			<div class="sidebar-header">
+				<h3>Hamelot</h3>
+			</div>
+
+			<ul class="list-unstyled components">
+				<li>
+					<a href="#userSubmenu" data-toggle="collapse" aria-expanded="false">
+						<i class="glyphicon glyphicon-home"></i>
+						کاربران
+					</a>
+					<ul class="collapse list-unstyled" id="userSubmenu">
+						<li>
+							<a href="#"> کاربر جدید</a>
+						</li>
+						<li>
+							<a href="#"> لیست کل</a>
+						</li>
+
+					</ul>
+				</li>
+				<li>
+					<a href="#hospitalSubmenu" data-toggle="collapse" aria-expanded="false">
+						<i class="glyphicon glyphicon-duplicate"></i>
+						بیمارستان ها
+					</a>
+					<ul class="collapse list-unstyled" id="hospitalSubmenu">
+						<li>
+							<a href="#"> بیمارستان جدید</a>
+						</li>
+						<li>
+							<a href="#"> لیست بیمارستان ها</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#partSubmenu" data-toggle="collapse" aria-expanded="false">
+						<i class="glyphicon glyphicon-duplicate"></i>
+						بخش ها
+					</a>
+					<ul class="collapse list-unstyled" id="partSubmenu">
+						<li>
+							<a href="#"> بخش جدید</a>
+						</li>
+						<li>
+							<a href="#"> لیست بخش ها</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#drSubmenu" data-toggle="collapse" aria-expanded="false">
+						<i class="glyphicon glyphicon-duplicate"></i>
+						پزشکان
+					</a>
+					<ul class="collapse list-unstyled" id="drSubmenu">
+						<li>
+							<a href="#"> پزشک جدید</a>
+						</li>
+						<li>
+							<a href="#"> لیست پزشکان</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#nurseSubmenu" data-toggle="collapse" aria-expanded="false">
+						<i class="glyphicon glyphicon-duplicate"></i>
+						پرستاران
+					</a>
+					<ul class="collapse list-unstyled" id="nurseSubmenu">
+						<li>
+							<a href="#"> پرستار جدید</a>
+						</li>
+						<li>
+							<a href="#"> لیست پرستاران</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#patientSubmenu" data-toggle="collapse" aria-expanded="false">
+						<i class="glyphicon glyphicon-duplicate"></i>
+						بیماران
+					</a>
+					<ul class="collapse list-unstyled" id="patientSubmenu">
+						<li>
+							<a href="#"> بیمار جدید</a>
+						</li>
+						<li>
+							<a href="#"> لیست بیماران</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#patientSubmenu" data-toggle="collapse" aria-expanded="false">
+						<i class="glyphicon glyphicon-duplicate"></i>
+						آزمایشات
+					</a>
+					<ul class="collapse list-unstyled" id="patientSubmenu">
+						<li>
+							<a href="#"> آزمایش جدید</a>
+						</li>
+						<li>
+							<a href="#"> لیست آزمایشات</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<!-- Page Content Holder -->
+		<div id="content" style="width:100%">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header navbar-right">
+						<button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
+							<i class="glyphicon glyphicon-align-left"></i>
+							<!-- <span>Toggle</span> -->
+						</button>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-left">
+							<li>
+								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+									@csrf
+								</form>
+								<a class="exit" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('general.logout') }}</a>
+							</li>
+							<li>
+								<a href="#">{{Auth::user()->prefix}} {{ Auth::user()->first_name }} {{Auth::user()->last_name}}</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div class="container">
+				@yield('content')
+			</div>
+		</div>
+	</div>
+	<footer class="container-fluid bg-4 text-center">
+		<p style="margin-top:15px;color:white;">design by
+			<a href="https://github.com/Hapoly">hapoly</a>
+		</p>
+	</footer>
+	<!-- jQuery CDN -->
+	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<!-- Bootstrap Js CDN -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$('#sidebarCollapse').on('click', function () {
+				$('#sidebar').toggleClass('active');
+			});
+		});
+	</script>
+</body>
+
+</html>
