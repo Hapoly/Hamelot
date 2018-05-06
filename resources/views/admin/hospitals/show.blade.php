34c23355a0eb2ae2d97ac634ed1db9113e9cea27
mr-exception <darbandi1996@gmail.com>
2018-05-07 01:56:20 +0430

    show injected

--- resources/views/admin/hospitals/show.blade.php
+++ resources/views/admin/hospitals/show.blade.php
@@ -1,89 +1,114 @@
-@extends('layouts.app')
+@extends('layouts.main')
 @section('title', $hospital->title)
 @section('content')
 <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">{{ $hospital->title }}</div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-9">{{$hospital->title}}</div>
-            <div class="col-md-3">:{{__('hospitals.title')}}</div>
-          </div>
-          <div class="row">
-            <div class="col-md-9">{{$hospital->address}}</div>
-            <div class="col-md-3">:{{__('hospitals.address')}}</div>
-          </div>
-          <div class="row">
-            <div class="col-md-9">{{$hospital->phone}}</div>
-            <div class="col-md-3">:{{__('hospitals.phone')}}</div>
-          </div>
-          <div class="row">
-            <div class="col-md-9">{{$hospital->mobile}}</div>
-            <div class="col-md-3">:{{__('hospitals.mobile')}}</div>
-          </div>
-          <div class="row">
-            <div class="col-md-9">{{$hospital->status_str()}}</div>
-            <div class="col-md-3">:{{__('hospitals.status')}}</div>
-          </div>
-          <div class="row">
-            <img src="{{asset($hospital->image)}}" />
-          </div>
-          <div class="row">
-            <a href="{{route('admin.hospitals.edit', ['hospital' => $hospital])}}" class="btn btn-primary" role="button">{{__('hospitals.edit')}}</a>
-            <form action="{{route('admin.hospitals.destroy', ['hospital' => $hospital])}}" method="post">
-              {{ method_field('DELETE') }}
-              {{ csrf_field() }}
-              <button type="submit" class="btn btn-danger">حذف</button>
-            </form>
-          </div>
-      </div>
+  <div class="card">
+    <div class="page-header">
+      <h2>{{ $hospital->title }}</h2>      
     </div>
-    <div class="col-md-8" style="margin-top: 1rem">
-      <a href="{{route('admin.departments.create')}}" class="btn btn-info" role="button">{{__('departments.create')}}</a>
-    </div>
-    <div class="col-md-8" style="margin-top: 1rem">
-      <div class="card">
-        <div class="card-header">{{__('departments.index_title')}}</div>
-        <div class="card-body">
-          @if(sizeof($hospital->departments))
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>{{__('departments.row')}}</th>
-                  <th>{{__('departments.title')}}</th>
-                  <th>{{__('departments.status')}}</th>
-                  <th>{{__('departments.operation')}}</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach($hospital->departments as $department)
-                  <tr>
-                    <td>{{$department->id}}</td>
-                    <td><a href="{{route('admin.departments.show', ['department' => $department])}}">{{$department->title}}</a></td>
-                    <td>{{$department->status_str()}}</td>
-                    <td>
-                      <form action="{{route('admin.departments.destroy', ['department' => $department])}}" style="display: inline" method="POST" class="trash-icon">
-                        {{ method_field('DELETE') }}
-                        {{ csrf_field() }}
-                        <button type="submit" class="btn btn-danger">{{__('departments.remove')}}</button>
-                      </form>
-                    </td>
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
-          @else
-            <div class="row">
-              <div class="col-md-12" style="text-align: center">
-                {{__('department_users.not_found')}}
-              </div>
-            </div>
-          @endif
+    <form class="myform" >
+      <img src="{{asset($hospital->image)}}" class="center">
+      <div class="form-group">
+        <div class="row">
+          <div class="col-sm-2">
+            <label class="first" for="email">{{__('hospitals.title')}}:</label>
+          </div>
+          <div class="col-sm-10">
+            <p class="first">{{$hospital->title}}</p>
+          </div>
         </div>
       </div>
+      <div class="form-group">
+        <div class="row">
+          <div class="col-sm-2">
+            <label class="other" for="email">{{__('hospitals.address')}}:</label>
+            </div>
+              <div class="col-sm-10">
+                <p class="other">{{$hospital->address}}</p>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="col-sm-2">
+                <label class="other" for="email">{{__('hospitals.phone')}}:</label>
+              </div>
+              <div class="col-sm-10">
+                <p class="other">{{$hospital->phone}}</p>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="col-sm-2">
+                <label class="other" for="email">{{__('hospitals.mobile')}}:</label>
+              </div>
+              <div class="col-sm-10">
+                <p class="other">{{$hospital->mobile}}</p>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="col-sm-2">
+                <label class="other" for="email">{{__('hospitals.status')}}:</label>
+              </div>
+              <div class="col-sm-10">
+                <p class="other">{{$hospital->status}}</p>
+              </div>
+            </div>
+          </div>
+          <a href="{{route('admin.hospitals.edit', ['hospital' => $hospital])}}" class="btn btn-primary" role="button">{{__('hospitals.edit')}}</a>
+          <form action="{{route('admin.hospitals.destroy', ['hospital' => $hospital])}}" method="post">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <button type="submit" class="btn btn-danger">حذف</button>
+          </form>
+        </div>
+      </div>  
+    </form>
+  </div>
+  <div class="card">
+    <div class="">
+      <h2>{{__('departments.index_title')}}</h2>      
     </div>
+    <a href="{{route('admin.departments.create')}}" class="btn add" style="float:left;margin-left:20px;">{{__('departments.create')}}
+      <i class="fa fa-plus"></i>
+    </a>
+    @if(sizeof($hospital->departments))
+      <table class="table">
+        <thead>
+          <tr>
+            <th>{{__('departments.row')}}</th>
+            <th>{{__('departments.title')}}</th>
+            <th>{{__('departments.status')}}</th>
+            <th>{{__('departments.operation')}}</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($hospital->departments as $department)
+            <tr>
+              <td>{{$department->id}}</td>
+              <td><a href="{{route('admin.departments.show', ['department' => $department])}}">{{$department->title}}</a></td>
+              <td>{{$department->status_str()}}</td>
+              <td>
+                <form action="{{route('admin.departments.destroy', ['department' => $department])}}" style="display: inline" method="POST" class="trash-icon">
+                  {{ method_field('DELETE') }}
+                  {{ csrf_field() }}
+                  <button type="submit" class="btn btn-danger">{{__('departments.remove')}}</button>
+                </form>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    @else
+      <div class="row">
+        <div class="col-md-12" style="text-align: center">
+          {{__('department_users.not_found')}}
+        </div>
+      </div>
+    @endif
   </div>
 </div>
 @endsection
