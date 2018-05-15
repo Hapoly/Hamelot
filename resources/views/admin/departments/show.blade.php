85ccb2c0175ece53d4d7c70d5074301e823b1e27
mr-exception <darbandi1996@gmail.com>
2018-05-15 06:58:54 +0430

    department show and create completed

--- resources/views/admin/departments/show.blade.php
+++ resources/views/admin/departments/show.blade.php
@@ -1,36 +1,43 @@
-@extends('layouts.app')
+@extends('layouts.main')
 @section('title', $department->title)
 @section('content')
 <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">{{ $department->title }}</div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-9">{{$department->title}}</div>
-            <div class="col-md-3">:{{__('departments.title')}}</div>
-          </div>
-          <div class="row">
-            <div class="col-md-9">{{$department->description}}</div>
-            <div class="col-md-3">:{{__('departments.description')}}</div>
-          </div>
-          <div class="row">
-            <div class="col-md-9"><a href="{{route('admin.hospitals.show', ['hospital' => $department->hospital])}}">{{$department->hospital->title}}</a></div>
-            <div class="col-md-3">:{{__('departments.hospital_id')}}</div>
-          </div>
-          <div class="row">
-            <div class="col-md-9">{{$department->status_str()}}</div>
-            <div class="col-md-3">:{{__('departments.status')}}</div>
-          </div>
-          <div class="row">
-            <a href="{{route('admin.departments.edit', ['department' => $department])}}" class="btn btn-primary" role="button">{{__('departments.edit')}}</a>
-            <form action="{{route('admin.departments.destroy', ['department' => $department])}}" method="post">
-              {{ method_field('DELETE') }}
-              {{ csrf_field() }}
-              <button type="submit" class="btn btn-danger">حذف</button>
-            </form>
-          </div>
+  <div class="panel panel-default">
+    <div class="row">
+      <h2>{{ $department->title }}</h2>
+    </div>
+    <div class="row">
+      <table class="table table-striped">
+        <tbody>
+          <tr>
+            <td>{{__('departments.title')}}</td>
+            <td>{{$department->title}}</td>
+          </tr>
+          <tr>
+            <td>{{__('departments.description')}}</td>
+            <td>{{$department->description}}</td>
+          </tr>
+          <tr>
+            <td>{{__('departments.hospital_id')}}</td>
+            <td><a href="{{route('admin.hospitals.show', ['hospital' => $department->hospital])}}">{{$department->hospital->title}}</a></td>
+          </tr>
+          <tr>
+            <td>{{__('departments.status')}}</td>
+            <td>{{$department->status_str()}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="row">
+      <div class="col-md-6" style="text-align: center">
+        <a href="{{route('admin.departments.edit', ['department' => $department])}}" class="btn btn-primary" role="button">{{__('departments.edit')}}</a>
+      </div>
+      <div class="col-md-6" style="text-align: center">
+        <form action="{{route('admin.departments.destroy', ['department' => $department])}}" method="post">
+          {{ method_field('DELETE') }}
+          {{ csrf_field() }}
+          <button type="submit" class="btn btn-danger">حذف</button>
+        </form>
       </div>
     </div>
   </div>
