5a2bb39bb9f427c95eedcd071264663773d9533d
sadafN <n.shell120@gmail.com>
2018-07-08 11:13:03 +0430

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <!-- <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,7 +15,7 @@
                     @endif
 
                     You are logged in!
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
