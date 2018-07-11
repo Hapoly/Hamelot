70ef0a79ba8f2110706deb6d768bb54230d6b010
sadafN <n.shell120@gmail.com>
2018-07-11 17:20:55 +0430

    patients create page compeleted

--- resources/views/admin/patients/create.blade.php
+++ resources/views/admin/patients/create.blade.php
@@ -1,16 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.main')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('patients.create') }}</div>
-                <div class="card-body">
+    <div class="panel panel-default create-card">
+        <h2>{{ __('patients.create') }}</h2>
+             <div class="row">
+                <div class="col-md-12">
                     <form method="POST" action="{{ route('admin.patients.store') }}" enctype="multipart/form-data">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('patients.first_name') }}</label>
-                            <div class="col-md-6">
+                         @csrf
+                        <div class="form-group row create-form">
+                            <div class="col-md-10">
                                 <input id="first_name" type="text" class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" value="{{ old('first_name') }}" required autofocus>
                                 @if ($errors->has('first_name'))
                                     <span class="invalid-feedback">
@@ -18,10 +16,10 @@
                                     </span>
                                 @endif
                             </div>
+                            <label for="first_name" class="col-md-2 col-form-label text-center">{{ __('patients.first_name') }}</label>
                         </div>
-                        <div class="form-group row">
-                            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('patients.last_name') }}</label>
-                            <div class="col-md-6">
+                        <div class="form-group row create-form">
+                            <div class="col-md-10">
                                 <input id="last_name" type="text" class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" value="{{ old('last_name') }}" required autofocus>
                                 @if ($errors->has('last_name'))
                                     <span class="invalid-feedback">
@@ -29,10 +27,10 @@
                                     </span>
                                 @endif
                             </div>
+                            <label for="last_name"class="col-md-2 col-form-label text-center">{{ __('patients.last_name') }}</label>
                         </div>
-                        <div class="form-group row">
-                            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('patients.id_number') }}</label>
-                            <div class="col-md-6">
+                        <div class="form-group row create-form">
+                            <div class="col-md-10">
                                 <input id="id_number" type="text" class="form-control{{ $errors->has('id_number') ? ' is-invalid' : '' }}" name="id_number" value="{{ old('id_number') }}" required autofocus>
                                 @if ($errors->has('id_number'))
                                     <span class="invalid-feedback">
@@ -40,36 +38,30 @@
                                     </span>
                                 @endif
                             </div>
+                            <label for="id_number"class="col-md-2 col-form-label text-center">{{ __('patients.id_number') }}</label>
                         </div>
-                        <div class="form-group row">
-                            <label for="gender" class="col-md-4 col-form-label text-md-right">{{ __('patients.gender') }}</label>
+
+                        <div class="form-group row create-form">
                             <div class="col-md-6">
-                                <select class="form-control" name="gender" id="gender">
-                                    <option value="1">{{__('patients.gender_str.1')}}  </option>
-                                    <option value="2">{{__('patients.gender_str.2')}}  </option>
-                                </select>
-                                @if ($errors->has('gender'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('gender') }}</strong>
-                                    </span>
-                                @endif
+                                <div class="form-group row">
+                                <div class="col-md-10">
+                                    <select class="form-control" name="gender" id="gender" style="width:80%">
+                                        <option value="1">{{__('patients.gender_str.1')}}  </option>
+                                        <option value="2">{{__('patients.gender_str.2')}}  </option>
+                                    </select>
+                                    @if ($errors->has('gender'))
+                                        <span class="invalid-feedback">
+                                            <strong>{{ $errors->first('gender') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                 <label  for="gender" class="col-md-2 col-form-label text-center">{{ __('patients.gender') }}</label>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('patients.image') }}</label>
                             <div class="col-md-6">
-                                <input id="image" type="file" class="form-control{{ $errors->has('image') ? ' is-invalid' : '' }}" name="image" required>
-                                @if ($errors->has('image'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('image') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="status" class="col-md-4 col-form-label text-md-right">{{ __('patients.status') }}</label>
-                            <div class="col-md-6">
-                                <select class="form-control" name="status" id="status">
+                                <div class="form-group row" style="margin-right:10px;">
+                                <div class="col-md-10">
+                                    <select class="form-control" name="status" id="status" style="width:80%;">
                                     <option value="1">{{__('patients.status_str.1')}}  </option>
                                     <option value="2">{{__('patients.status_str.2')}}  </option>
                                 </select>
@@ -78,15 +70,27 @@
                                         <strong>{{ $errors->first('status') }}</strong>
                                     </span>
                                 @endif
+                                </div>
+                                <label for="status" class="col-md-2 col-form-label text-center">{{ __('patients.status') }}</label>
+                                </div>
                             </div>
                         </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-12">
-                                <button type="submit" name="action" value="new" class="btn btn-primary">
-                                    {{ __('patients.save') }}
-                                </button>
+                        <div class="form-group row create-form">
+                            <div class="col-md-10">
+                                <input id="image" type="file" class="form-control{{ $errors->has('image') ? ' is-invalid' : '' }}" name="image" required>
+                                @if ($errors->has('image'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('image') }}</strong>
+                                    </span>
+                                @endif
                             </div>
+                            <label for="image" class="col-md-2 col-form-label text-center">{{ __('patients.image') }}</label>
                         </div>
+                              
+                        <button type="submit" name="action" value="new" class="btn btn-primary save-btn">
+                            {{ __('patients.save') }}
+                        </button>
+ 
                     </form>
                 </div>
             </div>
