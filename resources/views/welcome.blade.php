a10985172ff3056bf1be6945e2051a60a6817259
sadafN <n.shell120@gmail.com>
2018-09-23 10:59:54 +0330

    some little change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="search-things">
-                
+                <img src="/imgs/u4.png">
+               <form>
+                   <div class="form-group" style="padding:15px">
+                       <input type="text" class="form-control" placeholder="search">
+                   </div>
+               </form>
             </div>
         </div>
     </div>
