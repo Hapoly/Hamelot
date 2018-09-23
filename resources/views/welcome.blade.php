12ae179416ceb5a9fafc271804e21b65ddeb0be8
sadafN <n.shell120@gmail.com>
2018-09-23 18:52:14 +0330

    search page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,24 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="search-things">
-                <img src="/imgs/u4.png">
+                <img src="/imgs/logo.png">
                <form>
-                   <div class="form-group" style="padding:15px">
-                       <input type="text" class="form-control" placeholder="search">
+                   <div class="form-group" style="padding-top:25px">
+                       <input type="text" class="form-control search-input" style="width:80%;" placeholder="بیمارستان , درمانگاه , پزشک و...">
                    </div>
                </form>
+               <div class="row">
+                   <div class="col-md-6" style="text-align:left">
+                        <button type="button" class="btn search-btns">
+                            جستجو
+                        </button>
+                   </div>
+                   <div class="col-md-6" style="text-align:right">
+                        <button type="button" class="btn search-btns">
+                            اطراف من !
+                        </button>
+                   </div>
+               </div>
             </div>
         </div>
     </div>
