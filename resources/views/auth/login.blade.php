e796c305b2b6187089ef48aae989f5979022e667
sadafN <n.shell120@gmail.com>
2018-09-24 17:54:01 +0330

    some change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,62 +3,46 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-6 login-pic">
-            <img src="/imgs/logo.png" class="login-img">
-        </div>
-        <div class="col-md-6">
+        <div class="col-md-7">
             <div class="login-card">
-                <form class="login-form">
-
-                </form>
-            </div>
-        </div>
-        <div >
-            <div class="card login" style="margin-right: auto;margin-left: auto;">
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                <form class="login-form"method="POST" action="{{ route('login') }}">
                         @csrf
+                    <div class="form-group row">
+                        <label for="username" class="col-sm-3 col-form-label text-md-right">{{ __('general.username') }}</label>
 
-                        <div class="form-group row">
-                            <label for="username" class="col-sm-3 col-form-label text-md-right">{{ __('general.username') }}</label>
+                        <div class="col-md-8">
+                            <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
 
-                            <div class="col-md-8">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+                            @if ($errors->has('username'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('username') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="password" class="col-md-3 col-form-label text-md-right">{{ __('general.password') }}</label>
 
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="col-md-8">
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 offset-md-4">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('general.remember_me') }}
+                                </label>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-3 col-form-label text-md-right">{{ __('general.password') }}</label>
-
-                            <div class="col-md-8">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('general.remember_me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
+                    </div>
+                    <div class="form-group row mb-0">
                             <div class="col-md-3" style="text-align: center;">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('general.login') }}
@@ -70,9 +54,11 @@
                                 </a>
                             </div>
                         </div>
-                    </form>
-                </div>
+                </form>
             </div>
+        </div>
+        <div class="col-md-5 login-pic">
+            <img src="/imgs/logo.png" class="login-img">
         </div>
     </div>
 </div>
