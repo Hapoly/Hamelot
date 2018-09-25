6286e19cca5706ee6c8e3bc8ec030e6bb74c0d6d
sadafN <n.shell120@gmail.com>
2018-09-25 17:29:22 +0330

    login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,11 @@
     <div class="row">
         <div class="col-md-7">
             <div class="login-card">
-                <form class="login-form"method="POST" action="{{ route('login') }}">
+                <form class="login-form" method="POST" action="{{ route('register') }}">
                         @csrf
                     <div class="form-group row">
                         <label for="username" class="col-sm-3 col-form-label text-md-right">{{ __('general.username') }}</label>
-
+    
                         <div class="col-md-8">
                             <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
 
@@ -33,15 +33,28 @@
                             @endif
                         </div>
                     </div>
+
                     <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('general.remember_me') }}
-                                </label>
-                            </div>
+                        <label for="password-confirm" class="col-md-3 col-form-label text-md-right">{{ __('general.confirm_password') }}</label>
+
+                        <div class="col-md-8">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                         </div>
                     </div>
+                    <div class="form-group row">
+                        <label for="password-confirm" class="col-md-3 col-form-label text-md-right">{{ __('general.confirm_password') }}</label>
+
+                        <div class="col-md-8">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+                    <!-- inja -->
                     <div class="form-group row mb-0">
                             <div class="col-md-3" style="text-align: center;">
                                 <button type="submit" class="btn btn-primary">
@@ -64,51 +77,9 @@
 </div>
 
 
+           
 
-        <div class="col-md-8">
-            <div class="card register" style="margin-right: auto;margin-left: auto;">
-                <div class="card-header" style="background-color: #2a5477;color: white;text-align: right;">{{__('general.register') }}
-                </div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="username" class="col-md-3 col-form-label text-md-right">{{ __('general.username') }}</label>
-
-                            <div class="col-md-8">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
-
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-3 col-form-label text-md-right">{{ __('general.password') }}</label>
-
-                            <div class="col-md-8">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-3 col-form-label text-md-right">{{ __('general.confirm_password') }}</label>
-
-                            <div class="col-md-8">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
+     
 
                         <div class="form-group row">
                             <label for="first_name" class="col-md-3 col-form-label text-md-right">{{ __('general.first_name') }}</label>
