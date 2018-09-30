a6bb51b61772dc87ad7c348f9d620320c18193d6
sadafN <n.shell120@gmail.com>
2018-09-30 15:22:30 +0330

    tour changed

--- resources/views/tour.blade.php
+++ resources/views/tour.blade.php
@@ -1,4 +1,67 @@
 @extends('layouts.app')
 @section('content')
-tour
+<div class="tour-card">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+               
+            </div>
+            <div class="col-md-6">
+                <img src="/imgs/logo.png" class="tour-img">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="tour-card">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="/imgs/logo.png" class="tour-img">
+            </div>
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="tour-card">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                
+            </div>
+            <div class="col-md-6">
+                <img src="/imgs/logo.png" class="tour-img">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="tour-card">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="/imgs/logo.png" class="tour-img">
+            </div>
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="tour-card">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                
+            </div>
+            <div class="col-md-6">
+                <img src="/imgs/logo.png" class="tour-img">
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
