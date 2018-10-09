c9f434df9b7ce0a32d2db5da1957db74c876a2da
sadafN <n.shell120@gmail.com>
2018-10-09 16:31:05 +0330

    wallet and units card

--- resources/views/components/dashboard/units.blade.php
+++ resources/views/components/dashboard/units.blade.php
@@ -2,21 +2,21 @@
     use App\Models\Unit;
 @endphp
 <div class="col-md-4 col-sm-6 col-xs-12 pull-right">
-    <div class="panel panel-default">
+    <div class="panel panel-default units-panel">
         <div class="panel-body">
-            <div class="row">
+            <div class="row" style="margin-top:1rem;">
                 <div class="col-md-8 col-md-8 col-xs-8">
                     <div class="row">
                         <div class="col-md-12">
                             <span>واحد‌های درمانی: {{Unit::fetch(true)->count()}} عدد</span>
                         </div>
-                        <div class="col-md-12">
-                            <a class="btn btn-default" href="{{route('panel.units.create')}}">مشاهده واحد‌ها</a>
-                        </div>
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-4 col-xs-4">
                     <img src="{{asset('/imgs/004-hospital.svg')}}">
+                </div>
+                <div class="col-md-12">
+                    <a class="btn btn-default btn-units" href="{{route('panel.units.create')}}">مشاهده واحد‌ها</a>
                 </div>
             </div>
         </div>
--- resources/views/components/dashboard/wallet.blade.php
+++ resources/views/components/dashboard/wallet.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-4 col-sm-6 col-xs-12 pull-right">
-    <div class="panel panel-default">
+    <div class="panel panel-default wallet-panel">
         <div class="panel-body">
-            <div class="row">
+            <div class="row" style="margin-top:1rem;">
                 <div class="col-md-8 col-md-8 col-xs-8">
                     <div class="row">
                         <div class="col-md-12">
@@ -11,16 +11,17 @@
                                 <span>قابل برداشت: {{Auth::user()->avialable_credit()}} تومان</span>
                             @endif
                         </div>
-                        @if(Auth::user()->isManager())
-                            <div class="col-md-12">
-                                <a class="btn btn-default" href="{{route('panel.transactions.create.withdraw')}}">تسویه حساب</a>
-                            </div>
-                        @endif
+                      
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-4 col-xs-4">
                     <img src="{{asset('/imgs/001-wallet.svg')}}">
                 </div>
+                @if(Auth::user()->isManager())
+                    <div class="col-md-12">
+                        <a class="btn btn-default btn-checkout" href="{{route('panel.transactions.create.withdraw')}}">تسویه حساب</a>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
