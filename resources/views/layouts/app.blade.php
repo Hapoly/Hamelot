4f84f326321754bf102bfc860005306491606ec1
mr-exception <darbandi1996@gmail.com>
2018-10-14 09:34:34 +0330

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,14 +9,16 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
-
+    @php
+        use Illuminate\Support\Facades\Storage;
+    @endphp
     <!-- Scripts -->
     <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
  
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="{{ asset('css/app.css') }}?v={{hash_file('md5', app()->runningInConsole()?'public/css/app.css': 'css/app.css')}}" rel="stylesheet">
-    <link href="{{ asset('css/login.css') }}?v={{hash_file('md5', app()->runningInConsole()?'public/css/login.css': 'css/login.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?v={{hash_file('md5', Storage::disk('public')->path('css/app.css'))}}" rel="stylesheet">
+    <link href="{{ asset('css/login.css') }}?v={{hash_file('md5', Storage::disk('public')->path('css/login.css'))}}" rel="stylesheet">
     
 	<!-- jQuery UI CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
