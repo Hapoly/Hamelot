ae3cf7b46373ecb12134f348f72c1a0485b99311
mr-exception <darbandi1996@gmail.com>
2018-11-28 17:14:13 +0330

    merged

--- resources/views/components/forms/inputs/currency.blade.php
+++ resources/views/components/forms/inputs/currency.blade.php
@@ -2,7 +2,6 @@
 <div class="form-group row create-form">
 @else
 <div class="col-md-{{isset($col)? $col : 12}}">
-<<<<<<< HEAD
 @endif
     @if(isset($help))
         <div class="popup" id="popup{{$name}}"><i class="fa fa-question-circle" aria-hidden="true"></i>
@@ -11,9 +10,6 @@
             </span>
         </div>
     @endif
-=======
-   
->>>>>>> cf95b2530346da5c1ecc6ad6d1acf9ad67a61936
     <label for="{{$name}}" class="col-md-2 col-form-label text-center" style="float:right;">{{$label}}</label>
     <div class="col-md-1" style="padding:0;">
         @if(isset($help))
--- resources/views/components/forms/inputs/select.blade.php
+++ resources/views/components/forms/inputs/select.blade.php
@@ -2,7 +2,6 @@
 <div class="form-group row create-form">
 @else
 <div class="col-md-{{isset($col)? $col : 12}}">
-<<<<<<< HEAD
 @endif
     @if(isset($help))
         <div class="popup" id="popup{{$name}}"><i class="fa fa-question-circle" aria-hidden="true"></i>
@@ -14,21 +13,6 @@
     <label for="{{$name}}" class="col-md-{{isset($col)?3:2}} col-form-label text-center" style="float:right">{{$label}}</label>
     <div class="col-md-{{isset($col)?9:10}}">
         <select class="form-control" name="{{$name}}" id="{{$name}}" style="width:{{isset($col)?87:93}}%" {{isset($disabled)? ($disabled? 'disabled': ''): ''}}>
-=======
-   
-    <label for="{{$name}}" class="col-md-2 col-form-label text-center" style="float:right">{{$label}}</label>
-    <div class="col-md-1" style="padding:0;">
-        @if(isset($help))
-            <div class="popup" id="popup{{$name}}"><i class="fa fa-question-circle" aria-hidden="true"></i>
-                <span class="popuptext" id="help{{$name}}">
-                    {{$help}}
-                </span>
-            </div>
-        @endif
-    </div>
-    <div class="col-md-9" style="padding-left:0;">
-        <select class="form-control" name="{{$name}}" id="{{$name}}" style="width:93%" {{isset($disabled)? ($disabled? 'disabled': ''): ''}}>
->>>>>>> cf95b2530346da5c1ecc6ad6d1acf9ad67a61936
             @foreach($rows as $row)
                 <option value="{{$row['value']}}" {{$value == $row['value'] ? 'selected': ''}}>{{$row['label']}}</option>
             @endforeach
