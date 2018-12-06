53dcb5bcad250608cfb0a25c59c24b27637c74f7
mr-exception <darbandi1996@gmail.com>
2018-12-06 13:46:24 +0330

    rychat added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -110,6 +110,7 @@
 			$(".test").prepend('#testcard');   
 		}
 	</script>
+	<script type="text/javascript">!function(){function t(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,localStorage.getItem("rayToken")?t.src="https://app.raychat.io/scripts/js/"+o+"?rid="+localStorage.getItem("rayToken")+"&href="+window.location.href:t.src="https://app.raychat.io/scripts/js/"+o;var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=document,a=window,o="d7390c4b-6060-4f5e-8d88-4ea1cf417d15";"complete"==e.readyState?t():a.attachEvent?a.attachEvent("onload",t):a.addEventListener("load",t,!1)}();</script>
 </body>
 
 </html>
