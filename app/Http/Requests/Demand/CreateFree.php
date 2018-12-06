4643650939eb85d6eb7f0ff7a424158af899efeb
mr-exception <darbandi1996@gmail.com>
2018-12-06 13:48:34 +0330

    bug fi

--- app/Http/Requests/Demand/CreateFree.php
+++ app/Http/Requests/Demand/CreateFree.php
@@ -27,7 +27,7 @@ class CreateFree extends PersianFormRequest
     {
         return [
             'description'   => 'required|string',
-            'address_id'    => ['required', new UUID],
+            'address_id'    => ['nullable', new UUID],
             'asap'          => 'required|numeric|in:0,1',
             'start_time'    => 'required_if:asap,0|string',
             'end_time'      => 'required_if:asap,0|string',
