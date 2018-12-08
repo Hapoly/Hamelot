d744acfb1bb4c009d83750d623560181c7b4f756
sadafN <n.shell120@gmail.com>
2018-12-08 17:57:28 +0330

    welcom page changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,17 +6,25 @@
         <div class="col-md-8">
             <div class="search-things">
                 <img src="{{asset('/imgs/512.png')}}">
-                <h2 style="padding-top:20px;">
+                <h2 style="padding-top:20px;padding-bottom:20px;">
                 نوبت دهی دکتر سوال
                 </h2>
                 <form action="{{route('search')}}" method="GET">
-                    <div class="form-group" style="padding-top:25px">
+                    <!-- <div class="form-group" style="padding-top:25px">
                         <input type="text" name="term" class="form-control search-input" style="width:80%;" 
                         placeholder="جستجوی  پزشک ، بیمارستان ، درمانگاه  ">
                     </div>
                     <button type="submit" class="btn search-btns">
                         جستجو
-                    </button>
+                    </button> -->
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="جستجوی  پزشک ، بیمارستان ، درمانگاه  ">
+                        <div class="input-group-append">
+                        <button class="btn search-btns" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
