fcd4f48abe7972ad30704bf6f2ca94674579f13a
mr-exception <darbandi1996@gmail.com>
2018-12-21 02:07:37 +0330

    some permissions changed

--- resources/views/panel/units/edit.blade.php
+++ resources/views/panel/units/edit.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
   @form_edit(['action' => route('panel.units.update', ['unit' => $unit]), 'title' => __('units.edit')])
-    @input_text(['name' => 'title', 'value' => old('title', $unit->title), 'label' => __('units.title'), 'required' => true])
-    @input_text(['name' => 'slug', 'value' => old('slug', ''), 'label' => __('units.slug'), 'required' => true])
+    @input_text(['name' => 'title', 'value' => old('title', $unit->title), 'label' => __('units.title'), 'required' => true, 'row' => true])
+    @input_text(['name' => 'slug', 'value' => old('slug', $unit->slug), 'label' => __('units.slug'), 'required' => true, 'row' => true])
     <?php
         $parent_id_rows = [
           [
@@ -19,11 +19,11 @@
           ]);
         }
     ?>
-    @input_select(['name' => 'parent_id', 'value' => old('parent_id', $unit->parent_id), 'label' => __('units.parent_id'), 'required' => true, 'rows' => $parent_id_rows])
-    @input_text(['name' => 'slug', 'value' => old('slug', $unit->slug), 'label' => __('units.slug'), 'required' => true])
-    @input_text(['name' => 'address', 'value' => old('address', $unit->address), 'label' => __('units.address'), 'required' => true])
-    @input_text(['name' => 'phone', 'value' => old('phone', $unit->phone), 'label' => __('units.phone'), 'required' => false])
-    @input_text(['name' => 'mobile', 'value' => old('mobile', $unit->mobile), 'label' => __('units.mobile'), 'required' => false])
+    @input_select(['name' => 'parent_id', 'value' => old('parent_id', $unit->parent_id), 'label' => __('units.parent_id'), 'required' => true, 'rows' => $parent_id_rows, 'row' => true])
+    @input_text(['name' => 'slug', 'value' => old('slug', $unit->slug), 'label' => __('units.slug'), 'required' => true, 'row' => true])
+    @input_text(['name' => 'address', 'value' => old('address', $unit->address), 'label' => __('units.address'), 'required' => true, 'row' => true])
+    @input_text(['name' => 'phone', 'value' => old('phone', $unit->phone), 'label' => __('units.phone'), 'required' => false, 'row' => true])
+    @input_text(['name' => 'mobile', 'value' => old('mobile', $unit->mobile), 'label' => __('units.mobile'), 'required' => false, 'row' => true])
     @if(Auth::user()->isAdmin())
       <?php
           $status_rows = [
@@ -31,19 +31,19 @@
           [ 'value' => 2, 'label' => __('units.status_str.2') ],
           ];
       ?>
-      @input_select(['name' => 'status', 'value' => old('status', $unit->status), 'label' => __('units.status'), 'required' => true, 'rows' => $status_rows])
+      @input_select(['name' => 'status', 'value' => old('status', $unit->status), 'label' => __('units.status'), 'required' => true, 'rows' => $status_rows, 'row' => true])
     @else
       <input hidden name="status" value="{{$unit->status}}" />
     @endif
-    @input_image(['name' => 'image', 'label' => __('units.image'), 'required' => true])
-    @input_city(['city_id' => old('city_id', $unit->city_id), 'lon' => old('lon', $unit->lon), 'lat' => old('lat', $unit->lat), 'province_id' => old('province_id', $unit->city->province_id)])
+    @input_image(['name' => 'image', 'label' => __('units.image'), 'required' => true, 'row' => true])
+    @input_city(['city_id' => old('city_id', $unit->city_id), 'lon' => old('lon', $unit->lon), 'lat' => old('lat', $unit->lat), 'province_id' => old('province_id', $unit->city->province_id), 'row' => true])
     <?php
         $status_rows = [
           [ 'value' => 1, 'label' => __('units.status_str.1') ],
           [ 'value' => 2, 'label' => __('units.status_str.2') ],
         ];
     ?>
-    @input_select(['name' => 'status', 'value' => old('status', $unit->status), 'label' => __('units.status'), 'required' => true, 'rows' => $status_rows])
+    @input_select(['name' => 'status', 'value' => old('status', $unit->status), 'label' => __('units.status'), 'required' => true, 'rows' => $status_rows, 'row' => true])
     <?php
         $group_code_rows = [
           [ 'value' => 1, 'label' => __('units.group_code_str.1') ],
@@ -53,21 +53,21 @@
           [ 'value' => 5, 'label' => __('units.group_code_str.5') ],
         ];
     ?>
-    @input_select(['name' => 'group_code', 'value' => old('group_code', $unit->group_code), 'label' => __('units.group_code'), 'required' => true, 'rows' => $group_code_rows])
+    @input_select(['name' => 'group_code', 'value' => old('group_code', $unit->group_code), 'label' => __('units.group_code'), 'required' => true, 'rows' => $group_code_rows, 'row' => true])
     <?php
         $type_rows = [
           [ 'value' => 1, 'label' => __('units.type_str.1') ],
           [ 'value' => 2, 'label' => __('units.type_str.2') ],
         ];
     ?>
-    @input_select(['name' => 'type', 'value' => old('type', $unit->type), 'label' => __('units.type'), 'required' => true, 'rows' => $type_rows])
+    @input_select(['name' => 'type', 'value' => old('type', $unit->type), 'label' => __('units.type'), 'required' => true, 'rows' => $type_rows, 'row' => true])
     <?php
         $public_rows = [
           [ 'value' => 1, 'label' => __('units.public_str.1') ],
           [ 'value' => 2, 'label' => __('units.public_str.2') ],
         ];
     ?>
-    @input_select(['name' => 'public', 'value' => old('public', $unit->public), 'label' => __('units.public'), 'required' => true, 'rows' => $public_rows])
+    @input_select(['name' => 'public', 'value' => old('public', $unit->public), 'label' => __('units.public'), 'required' => true, 'rows' => $public_rows, 'row' => true])
     @submit_row(['value' => 'edit', 'label' => __('units.save')])
   @endform_create
 </div>
